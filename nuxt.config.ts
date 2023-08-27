6ca5817f2fa737667b8fb7f68680f1cd49e8b020
jacoacoacob <jacoacoacob@protonmail.com>
2023-08-27 10:10:42 -0500

    first commit; installed and configured tailwind

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules: [
+        "@nuxt/content",
+    ],
+    css: [
+        "~/assets/main.css",
+    ],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+});
